e9d862b92deae109a28d5d084eac04771f9af195
anvesh-vankayala <42882800+anvesh-vankayala@users.noreply.github.com>
2024-10-26 16:32:04 +0000

    updated

--- mlops_week7/src/train.py
+++ mlops_week7/src/train.py
@@ -1,72 +1,151 @@
 import os
 from pathlib import Path
-import argparse
+import logging
+import comet_ml
 
+import hydra
+import torch
 import lightning as L
-from lightning.pytorch.callbacks import ModelCheckpoint, RichProgressBar, RichModelSummary
-from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.loggers import Logger 
+from typing import List
+from omegaconf import DictConfig
+from lightning import seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint
 
-from datamodules.dogbreed_datamodule import DogBreedImageDataModule
-from models.timm_classifier import TimmClassifier
-from utils.logging_utils import setup_logger, task_wrapper
+import rootutils
 
-@task_wrapper
-def train_and_save(data_module, model, trainer, save_path="../model_storage/model.ckpt"):
-    trainer.fit(model, data_module)
-    # trainer.test(model, data_module)
-    # model.save_model(save_path)
-    # print(f"Model saved to {save_path}")
+root = rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+from src.utils.logging_utils import setup_logger, task_wrapper
+
+log = logging.getLogger(__name__)
+
 
 class CustomModelCheckpiont(ModelCheckpoint):
     def _save_checkpoint(self, trainer, filepath):
         trainer.lightning_module.save_transformed_model = True
         super()._save_checkpoint(trainer, filepath)
-        # print(filepath)
 
-def main(args):
-    # Set up paths
-    base_dir = Path(__file__).resolve().parent.parent
-    data_dir = base_dir / args.data
-    log_dir = base_dir / args.logs
-    ckpt_path = base_dir / args.ckpt_path
+
+def instantiate_callbacks(callback_cfg: DictConfig) -> List[L.Callback]:
+    callbacks: List[L.Callback] = []
+    if not callback_cfg:
+        log.warning("No callback configs found! Skipping..")
+        return callbacks
     
+    for _, cb_conf in callback_cfg.items():
+        if "_target_" in cb_conf:
+            log.info(f"Instantiating callback <{cb_conf._target_}>")
+            callbacks.append(hydra.utils.instantiate(cb_conf))
+    return callbacks
+
+def instantiate_loggers(logger_cfg: DictConfig) -> List[Logger]:
+    loggers: List[Logger] = []
+    if not logger_cfg:
+        log.warning("No logger configs found! Skipping..")
+        return loggers
+    
+    for _, lg_conf in logger_cfg.items():
+        if "_target_" in lg_conf:
+            log.info(f"instantiating logger <{lg_conf._target_}>")
+            loggers.append(hydra.utils.instantiate(lg_conf))
+    return loggers
+
+@task_wrapper
+def train_task(
+    cfg: DictConfig,
+    trainer: L.Trainer,
+    model: L.LightningModule,
+    datamodule: L.LightningDataModule,
+):
+    log.info("Starting training!")
+    print("#######training started####")
+    trainer.fit(model, datamodule)
+    train_metrics = trainer.callback_metrics
+    log.info(f"Training metrics:\n{train_metrics}")
+    return train_metrics
+
+
+@task_wrapper
+def test_task(
+    cfg: DictConfig,
+    trainer: L.Trainer,
+    model: L.LightningModule,
+    datamodule: L.LightningDataModule
+):
+    log.info("Starting testing!")
+    if cfg.callbacks.model_checkpoint.filename:
+        log.info(
+            f"Loading best checkpoint: {cfg.callbacks.model_checkpoint.filename}"
+        )
+        test_metrics = trainer.test(
+            model, datamodule, ckpt_path=cfg.callbacks.model_checkpoint.filename
+        )
+    else:
+        log.warning("No checkpoint found! Using current model weights.")
+        test_metrics = trainer.test(model, datamodule)
+    log.info(f"Test metrics:\n{test_metrics}")
+
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="train")
+def train(cfg: DictConfig):
+    print("cfg")
+    print(cfg)
+    # Set up paths
+    log_dir = Path(cfg.paths.log_dir)
+
     # Set up logger
     setup_logger(log_dir / "train_log.log")
 
     # Initialize DataModule
-    data_module = DogBreedImageDataModule(dl_path=data_dir, batch_size=32, num_workers=0)
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    datamodule: L.LightningDataModule = hydra.utils.instantiate(cfg.data)
+    # print("datamodule")
+    # print(len(datamodule.train_dataloader()))
+    # print(len(datamodule.train_dataloader()))
 
     # Initialize Model
-    model = TimmClassifier(lr=1e-3)
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: L.LightningModule = hydra.utils.instantiate(cfg.model)
 
-    # Set up checkpoint callback
-    checkpoint_callback = CustomModelCheckpiont(
-        dirpath=ckpt_path,
-        filename="{epoch}-checkpoint",
-        save_top_k=3,
-        monitor="val/loss",
-        mode="min",
-    )
+    # Set up callbacks
+    callbacks: List[L.Callback] = instantiate_callbacks(cfg.get("callbacks"))
+
+    # Set up loggers
+    loggers: List[Logger] = instantiate_loggers(cfg.get("logger"))
 
     # Initialize Trainer
-    trainer = L.Trainer(
-        max_epochs=1,
-        callbacks=[
-            checkpoint_callback,
-            RichProgressBar(),
-            RichModelSummary(max_depth=2)
-        ],
-        accelerator="auto",
-        logger=TensorBoardLogger(save_dir=log_dir, name="dogbreed_classification"),
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: L.Trainer = hydra.utils.instantiate(
+        cfg.trainer,
+        callbacks=callbacks,
+        logger=loggers,
     )
 
-    # Train and test the model
-    train_and_save(data_module, model, trainer)
+    # Train the model
+    train_metrics = {}
+
+    # Train the model
+    if cfg.get("train"):
+        train_metrics = train_task(cfg, trainer, model, datamodule)
+    print("train_metrics-", train_metrics)
+    # Test the model
+    test_metrics = {}
+    if cfg.get("test"):
+        test_metrics = test_task(cfg, trainer, model, datamodule)
+
+    # Combine metrics
+    # all_metrics = {**train_metrics, **test_metrics}
+    all_metrics = {**train_metrics}
+
+    # Extract and return the optimization metric
+    optimization_metric = all_metrics.get(cfg.get("optimization_metric"))
+    if optimization_metric is None:
+        log.warning(f"Optimization metric '{cfg.get('optimization_metric')}' not found in metrics. Returning 0.")
+        return 0.0
+    
+    return optimization_metric
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Infer using trained Dogbreed Classifier")
-    parser.add_argument("--data", type=str, required=True, help="Path to data containing images")
-    parser.add_argument("--logs", type=str, required=True, help="Path to logs")
-    parser.add_argument("--ckpt_path", type=str, required=True, help="Path to model checkpoint")
-    args = parser.parse_args()
-    main(args)
+    train()
