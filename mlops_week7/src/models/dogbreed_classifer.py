fd85f984f46f946079655b073fffb01b67b8f051
anvesh-vankayala <42882800+anvesh-vankayala@users.noreply.github.com>
2024-11-01 16:24:19 +0000

    updated

--- mlops_week7/src/models/dogbreed_classifer.py
+++ mlops_week7/src/models/dogbreed_classifer.py
@@ -4,36 +4,36 @@ import torch
 import torch.nn.functional as F
 from torch import optim
 from torchmetrics import Accuracy, MaxMetric
-from torchmetrics.classification import MulticlassConfusionMatrix
-import matplotlib.pyplot as plt
-import os
-import ast
-from glob import glob
 
-class DogBreedClassifier(L.LightningModule):
-    def __init__(self, base_model: str = "resnet18", pretrained=False, num_classes: int = 10,
-        lr: float = 1e-3, weight_decay: float = 0.0001, **kwargs):
+
+class TimmClassifier(L.LightningModule):
+    def __init__(
+        self,
+        base_model: str = "resnet18",
+        num_classes: int = 10,
+        pretrained: bool = True,
+        lr: float = 1e-3,
+        weight_decay: float = 1e-5,
+        factor: float = 0.1,
+        patience: int = 10,
+        min_lr: float = 1e-6,
+        **kwargs,
+    ):
         super().__init__()
-        self.lr = lr
-        self.num_classes = num_classes
-        self.patch_size = kwargs['patch_size']
-        self.embed_dim = kwargs['embed_dim']
-        print(kwargs)
-        # Load pre-trained ResNet18 model
-        self.model = timm.create_model(base_model, pretrained=pretrained, 
-            num_classes=self.num_classes, dims=ast.literal_eval(kwargs['dims']), depths=ast.literal_eval(kwargs['depths'])
-            )
-
-        # Multi-class accuracy with num_classes=2
-        self.train_acc = Accuracy(task="multiclass", num_classes=self.num_classes)
-        self.val_acc = Accuracy(task="multiclass", num_classes=self.num_classes)
-        self.test_acc = Accuracy(task="multiclass", num_classes=self.num_classes)
-        self.test_acc_best = MaxMetric()
-        self.validation_step_outputs = []
-        self.train_confusion =  MulticlassConfusionMatrix(num_classes=2)
-        self.test_confusion =  MulticlassConfusionMatrix(num_classes=2)
-
         self.save_hyperparameters()
+
+        # Load pre-trained model
+        self.model = timm.create_model(
+            base_model, pretrained=pretrained, num_classes=num_classes, **kwargs
+        )
+
+        # Multi-class accuracy
+        self.train_acc = Accuracy(task="multiclass", num_classes=num_classes)
+        self.val_acc = Accuracy(task="multiclass", num_classes=num_classes)
+        self.test_acc = Accuracy(task="multiclass", num_classes=num_classes)
+        
+        # Add MaxMetric for tracking best validation accuracy
+        self.val_acc_best = MaxMetric()
 
     def forward(self, x):
         return self.model(x)
@@ -44,25 +44,9 @@ class DogBreedClassifier(L.LightningModule):
         loss = F.cross_entropy(logits, y)
         preds = F.softmax(logits, dim=1)
         self.train_acc(preds, y)
-        if self.current_epoch == self.trainer.max_epochs - 1:
-            self.train_confusion(preds, y)
-        self.log("train_loss", loss, prog_bar=True, on_epoch=True)
-        self.log("train_acc", self.train_acc, prog_bar=True, on_epoch=True)
+        self.log("train/loss", loss, prog_bar=True)
+        self.log("train/acc", self.train_acc, prog_bar=True)
         return loss
-
-    def on_train_end(self):        
-        print('CONFUSION_MATRIX:', self.train_confusion.compute())
-        fig_, ax_ = self.train_confusion.plot()
-        
-        # Find the most recent metrics.csv file
-        eval_log_files = glob("logs/train/multiruns/*/*/train.log")
-        latest_eval_log = max(eval_log_files, key=os.path.getctime)
-        log_path = latest_eval_log.split("/train.log")[0]
-        print("log_path-", log_path)
-        fig_.savefig(os.path.join(log_path, "train_confusion_matrix.png"))
-
-        # Optionally, you can close the figure to free up memory
-        plt.close(fig_)
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -70,60 +54,17 @@ class DogBreedClassifier(L.LightningModule):
         loss = F.cross_entropy(logits, y)
         preds = F.softmax(logits, dim=1)
         self.val_acc(preds, y)
-        self.log("val_loss", loss, prog_bar=True, on_epoch=True)
-        self.log("val_acc", self.val_acc, prog_bar=True, on_epoch=True)
-    
-    def predict_step(self, batch, batch_idx):
-        # print(batch[0])
-        # print(kwargs)
-        # enable Monte Carlo Dropout
-        # self.dropout.train()
-        # print("batch")
-        # print(batch)
-        x = batch[0]
-        # take average of `self.mc_iteration` iterations
-        # pred = self.model(x).unsqueeze(0)
-        # pred = torch.vstack(pred).mean(dim=0)
-        pred = self(x)
-        # print("pred")
-        # print(pred)
-        predicted_classes = torch.argmax(pred, dim=1)
-        # print("predicted_classes", predicted_classes)
-        return predicted_classes, batch[2]
-        
+        self.log("val/loss", loss, prog_bar=True)
+        self.log("val/acc", self.val_acc, prog_bar=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = F.cross_entropy(logits, y)
         preds = F.softmax(logits, dim=1)
-        preds_conf = torch.argmax(preds, dim=1)
         self.test_acc(preds, y)
-        self.test_confusion(preds_conf, y)
-        self.log("test_loss", loss, prog_bar=True, on_epoch=True)
-        self.log("test_acc", self.test_acc, prog_bar=True, on_epoch=True)
-    
-    def on_test_epoch_end(self):
-        print('CONFUSION_MATRIX:', self.test_confusion.compute())
-        fig_, ax_ = self.test_confusion.plot()
-        
-        # Find the most recent metrics.csv file
-        # saving to train itself may be for eval.py you can change this depending on config
-        eval_log_files = glob("logs/train/multiruns/*/*/train.log")
-        latest_eval_log = max(eval_log_files, key=os.path.getctime)
-        log_path = latest_eval_log.split("/train.log")[0]
-        # print("log_path-", log_path)
-        fig_.savefig(os.path.join(log_path, "test_confusion_matrix.png"))
-
-        # Optionally, you can close the figure to free up memory
-        plt.close(fig_)
-
-
-    def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=self.lr)
-
-    def save_model(self, path):
-        torch.save(self.state_dict(), path)
+        self.log("test/loss", loss, prog_bar=True)
+        self.log("test/acc", self.test_acc, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(
@@ -143,7 +84,22 @@ class DogBreedClassifier(L.LightningModule):
             "optimizer": optimizer,
             "lr_scheduler": {
                 "scheduler": scheduler,
-                "monitor": "val_loss",
+                "monitor": "val/loss",
                 "interval": "epoch",
             },
         }
+
+    def on_validation_epoch_end(self):
+        acc = self.val_acc.compute()  # get current val acc
+        self.val_acc_best(acc)  # update best so far val acc
+        
+        # log `val_acc_best` as a value through `.compute()` method
+        self.log("val/acc_best", self.val_acc_best.compute(), prog_bar=True)
+        
+        # log `hp_metric` which will be used for storing the best score for hyperparameter optimization
+        self.log("hp_metric", self.val_acc_best.compute())
+
+    def on_train_start(self):
+        # by default lightning executes validation step sanity checks before training starts,
+        # so it's worth to make sure validation metrics don't store results from these checks
+        self.val_acc_best.reset()
