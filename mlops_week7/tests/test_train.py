79ad58fbff50354f8dee94a2d07deda71cee6ee1
anvesh-vankayala <42882800+anvesh-vankayala@users.noreply.github.com>
2024-11-03 09:12:05 +0000

    updated

--- mlops_week7/tests/test_train.py
+++ mlops_week7/tests/test_train.py
@@ -21,21 +21,33 @@ import time
 def config():
     with hydra.initialize(version_base=None, config_path="../configs"):
         cfg = hydra.compose(
-            config_name="train",
-            overrides=["experiment=dogbreed_ex_train","trainer.max_epochs=3"],
+            config_name="train.yaml",
+            overrides=["experiment=dogbreed_ex_train",
+                       "trainer.max_epochs=3",
+                       "test=True"],
         )
+
+        # Override paths in the config
+        project_root = rootutils.find_root(indicator=".project-root")
+        cfg.paths.root_dir = str(project_root)
+        cfg.paths.data_dir = str(project_root / "data")
+        cfg.paths.log_dir = str(project_root / "logs")
+        cfg.paths.output_dir = str(project_root / "outputs")
+        cfg.paths.work_dir = str(project_root)
         return cfg
 
 def parse_metrics_from_console_output(caplog):
     """Parse metrics from the captured console output."""
     for record in caplog.records:
-        if "'val_acc':" in record.getMessage():
+        if "'val/acc':" in record.getMessage():
+            print('Entered if condition >>>>>>>>>>>>>>>>>>>>>> -------------------')
             # Extract the dictionary-like string
             metrics_str = re.search(r'{.*}', record.getMessage()).group(0)
-            
+            print('metrics string --  -------------------',metrics_str)
             # Parse individual values using regex
             metrics = {}
-            pattern = r"'(\w+)': tensor\(([\d.]+)\)"
+            pattern = r"'([\w/]+)': tensor\(([\d.]+)\)"
+            # pattern = r"'(\w+)': tensor\(([\d.]+)\)"
             matches = re.finditer(pattern, metrics_str)
             
             for match in matches:
@@ -58,6 +70,7 @@ def test_dogbreed_ex_training(config, tmp_path, caplog):
     config.paths.output_dir = str(tmp_path)
     config.paths.log_dir = str(tmp_path / "logs")
     
+    print('>>>>>>>>>>>>>>>----->',config)
     # Run training
     train(config)
     
@@ -72,5 +85,6 @@ def test_dogbreed_ex_training(config, tmp_path, caplog):
     
     # Assert metrics were found and validation accuracy meets threshold
     assert metrics is not None, "Could not find metrics in console output"
-    val_acc = metrics['val_acc']
+    print(f'metrics >>>>  {metrics}')
+    val_acc = metrics['val/acc']
     assert val_acc > 0.30, f"Validation accuracy {val_acc} is not greater than 0.30"
